58fad4f4f89ee0cb6b8d5b8a55565c50190e42de
Howard Yang <you@example.com>
2018-03-10 16:54:04 -0800

    added submit

---
+++ assets/js/javascript.js
@@ -0,0 +1,11 @@
+// $(document).ready(function() {
+//     $('form').submit(function() {
+//       console.log('submit triggered');
+//       var email = $("[name=email]").val();
+//       var url = ""
+//
+//
+//     });
+// });
+
+// <div class="_form_1"></div><script src="https://shoobilee.activehosted.com/f/embed.php?id=1" type="text/javascript" charset="utf-8"></script>
