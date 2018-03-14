21611416da454b7079f842ff4dd1393eb44abc16
Howard Yang <you@example.com>
2018-03-13 21:58:13 -0700

    added btn disabled

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,11 +1,10 @@
-// $(document).ready(function() {
-//     $('form').submit(function() {
-//       console.log('submit triggered');
-//       var email = $("[name=email]").val();
-//       var url = ""
-//
-//
-//     });
-// });
-
-// <div class="_form_1"></div><script src="https://shoobilee.activehosted.com/f/embed.php?id=1" type="text/javascript" charset="utf-8"></script>
+$(document).ready(function() {
+  // $(':button[type="submit"]').prop('disabled', true);
+  $('input[type="email"]').keyup(function() {
+    if($(this).val() != '') {
+      $(':button[type="submit"]').prop('disabled', false);
+    }else{
+      $(':button[type="submit"]').prop('disabled', true);
+    }
+  });
+});
